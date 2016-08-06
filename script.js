c59024a37f8897808685c53ffb9143d61c824c2e
Akkuzin Maxim <akkmaxon2307@gmail.com>
2016-08-06 12:22:27 +0300

    Good resizing of projects

---
+++ script.js
@@ -0,0 +1,28 @@
+$spaceBetweenProjects = 24;
+$paddingContainer = 15;
+
+resizeProjects = function() {
+  $width = $(window).width();
+  if($width < 768) {
+    newWidth = $width - 2 * $paddingContainer;
+  } else if($width < 992) {
+    newWidth = $width / 2 - 2 * $paddingContainer;
+  } else {
+    newWidth = $width / 3 - 2 * $paddingContainer;
+  }
+  $('.project').css('width', newWidth);
+};
+
+$(window).load(function() {
+  resizeProjects();
+  $projects = $('#projects')
+  $projects.masonry({
+    itemSelector: '.project',
+    gutter: $spaceBetweenProjects,
+    isFitWidth: true
+  });
+});
+
+$(window).resize(function() {
+  resizeProjects();
+});
